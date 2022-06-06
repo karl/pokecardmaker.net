cbdf7cce420c0ce7e24c3748c39ebfa180bd7adb
Leon van der Noll <leonvdnoll@gmail.com>
2022-06-06 12:22:19 +0200

    added 100vh to the page

--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -2,11 +2,15 @@ import { styled } from '@css';
 import { Container } from '@mui/material';
 
 export const Background = styled('div')`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
   background: ${({ theme }) => theme.custom.backgroundGradient};
 `;
 
 export const MainContainer = styled(Container)`
   padding: ${({ theme }) => theme.spacing(5, 3, 3)};
+  flex-grow: 1;
 `;
 
 export const CardWrapper = styled('div')`
