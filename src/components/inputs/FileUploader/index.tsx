3990635b60918e5ccaa8dcf8acdc96af71870c23
Leon van der Noll <leonvdnoll@gmail.com>
2022-08-09 19:24:46 +0200

    added html-to-canvas fileuploader support

--- src/components/inputs/FileUploader/index.tsx
+++ src/components/inputs/FileUploader/index.tsx
@@ -1,6 +1,13 @@
 import { UploadFile as UploadFileIcon } from '@mui/icons-material';
-import { Button, FormControl } from '@mui/material';
-import { ChangeEvent, FC, useCallback, useEffect, useState } from 'react';
+import { Button, CircularProgress, FormControl } from '@mui/material';
+import {
+  ChangeEvent,
+  FC,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import Label from '../Label';
 import { ButtonLabel } from './styles';
 import { FileUploaderProps } from './types';
@@ -12,23 +19,52 @@ const FileUploader: FC<FileUploaderProps> = ({
   file,
   tooltipProps,
 }) => {
+  const fileReader = useRef<FileReader>();
+  const [isLoading, setLoading] = useState<boolean>(false);
   const [fileName, setFileName] = useState<string>();
 
   useEffect(() => {
     if (!file) setFileName(undefined);
   }, [file]);
 
-  const onUpload = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      const upload = e.currentTarget.files?.[0];
-      if (!upload) return;
-      // TODO: Convert the blobs to dataURL using FileReader
-      onChange(URL.createObjectURL(upload));
-      setFileName(upload.name);
-      e.currentTarget.value = '';
-    },
-    [onChange],
-  );
+  const onUpload = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const upload = e.currentTarget.files?.[0];
+    if (!upload) return;
+    setLoading(true);
+
+    // Read as dataURL so we don't have to use local blobs, blobs don't work with html-to-canvas
+    fileReader.current?.readAsDataURL(upload);
+    setFileName(upload.name);
+    e.currentTarget.value = '';
+  }, []);
+
+  // Subscribe to FileReader results and call the onChange when a result comes in
+  useEffect(() => {
+    if (!fileReader.current) {
+      fileReader.current = new FileReader();
+    }
+
+    const successCallback = () => {
+      const result = fileReader.current?.result;
+      if (typeof result !== 'string') return;
+
+      setLoading(false);
+      // TODO: Compress image to not be any bigger than cardWidth and cardHeight?
+      onChange(result);
+    };
+
+    const errorCallback = () => {
+      setLoading(false);
+    };
+
+    fileReader.current?.addEventListener('load', successCallback);
+    fileReader.current?.addEventListener('error', errorCallback);
+
+    return () => {
+      fileReader.current?.removeEventListener('load', successCallback);
+      fileReader.current?.removeEventListener('error', errorCallback);
+    };
+  }, [onChange]);
 
   return (
     <FormControl>
@@ -43,7 +79,13 @@ const FileUploader: FC<FileUploaderProps> = ({
         variant="outlined"
         color="inherit"
         component="label"
-        startIcon={<UploadFileIcon />}
+        startIcon={
+          isLoading ? (
+            <CircularProgress color="inherit" size={20} />
+          ) : (
+            <UploadFileIcon />
+          )
+        }
       >
         <ButtonLabel>{fileName ?? <>&nbsp;</>}</ButtonLabel>
         <input
