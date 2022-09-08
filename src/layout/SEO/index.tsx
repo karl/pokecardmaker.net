15249dfaaa1d0c6e2eef34bd3b1203d73fd04bcf
Leon van der Noll <leonvdnoll@gmail.com>
2022-09-08 11:00:19 +0200

    fixed SEO title for og and twitter

--- src/layout/SEO/index.tsx
+++ src/layout/SEO/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 
 interface SEOProps {
   description: string;
@@ -13,27 +13,34 @@ const SEO: FC<SEOProps> = ({
   title,
   siteTitle = 'Pokécardmaker.net',
   fullTitle,
-}) => (
-  <Head>
-    <title>{fullTitle ?? `${title} | ${siteTitle}`}</title>
-    <meta name="description" content={description} />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    <meta property="og:site_name" content={siteTitle} />
-    {/* TODO: Change this image to avoid cropping */}
-    <meta
-      property="og:image"
-      content="https://pokecardmaker.net/assets/images/metaImage.png"
-    />
-    <meta property="twitter:card" content="summary" />
-    <meta property="twitter:title" content={title} />
-    <meta property="twitter:description" content={description} />
-    <meta
-      property="twitter:image"
-      content="https://pokecardmaker.net/assets/images/metaImage.png"
-    />
-  </Head>
-);
+}) => {
+  const finalTitle = useMemo<string>(
+    () => fullTitle ?? `${title} | ${siteTitle}`,
+    [fullTitle, title, siteTitle],
+  );
+
+  return (
+    <Head>
+      <title>{finalTitle}</title>
+      <meta name="description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={finalTitle} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={siteTitle} />
+      {/* TODO: Change this image to avoid cropping */}
+      <meta
+        property="og:image"
+        content="https://pokecardmaker.net/assets/images/metaImage.png"
+      />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:title" content={finalTitle} />
+      <meta property="twitter:description" content={description} />
+      <meta
+        property="twitter:image"
+        content="https://pokecardmaker.net/assets/images/metaImage.png"
+      />
+    </Head>
+  );
+};
 
 export default SEO;
